02b31867b526962f4f02b2353a8dfb2b3644cb09
Ricardo Mestre <ricardo@duplos.org>
2019-07-17 15:51:42 +0100

    Structure and style ATBD view layout using Inpage components

--- src/components/atbds/View.js
+++ src/components/atbds/View.js
@@ -1,6 +1,21 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import T from 'prop-types';
 import { connect } from 'react-redux';
+import { StickyContainer, Sticky } from 'react-sticky';
+
+import {
+  Inpage,
+  InpageHeader,
+  InpageHeaderInner,
+  InpageHeadline,
+  InpageTitle,
+  InpageTagline,
+  InpageToolbar,
+  InpageBody,
+  InpageBodyInner
+} from '../common/Inpage';
+
+import Prose from '../../styles/type/prose';
 
 class AtbdView extends Component {
   static propTypes = {
@@ -15,32 +30,56 @@ class AtbdView extends Component {
     const { contacts } = atbd;
 
     return (
-      <Fragment>
+      <Inpage>
         {atbd && (
-          <Fragment>
-            <h1>{atbd.title}</h1>
-            <h2>Identifying Information</h2>
-            <h3>General</h3>
-            <h3>Citation</h3>
-            <h2>Introduction</h2>
-            <h3>Introduction</h3>
-            <h3>Historical Perspective</h3>
-            <h2>Contacts</h2>
-            {contacts && contacts.length === 0 ? (
-              <div>No contacts associated.</div>
-            ) : (
-              <ul>
-                {contacts.map(c => (
-                  <li key={c.contact_id}>{c.displayName}</li>
-                ))}
-              </ul>
-            )}
-            <h2>References</h2>
-            <h2>Algorithm Description</h2>
-            <h2>Algorithm Usage</h2>
-          </Fragment>
+          <StickyContainer>
+            <Sticky>
+              {stickyProps => (
+                <InpageHeader
+                  style={stickyProps.style}
+                  isSticky={stickyProps.isSticky}
+                >
+                  <InpageHeaderInner>
+                    <InpageHeadline>
+                      <InpageTitle>{atbd.title}</InpageTitle>
+                      <InpageTagline>Viewing document</InpageTagline>
+                    </InpageHeadline>
+                    <InpageToolbar>
+                      Toolbar
+                    </InpageToolbar>
+                  </InpageHeaderInner>
+                </InpageHeader>
+              )}
+            </Sticky>
+            <InpageBody>
+              <InpageBodyInner>
+                <Prose>
+                  <h1>{atbd.title}</h1>
+                  <h2>Identifying Information</h2>
+                  <h3>General</h3>
+                  <h3>Citation</h3>
+                  <h2>Introduction</h2>
+                  <h3>Introduction</h3>
+                  <h3>Historical Perspective</h3>
+                  <h2>Contacts</h2>
+                  {contacts && contacts.length === 0 ? (
+                    <div>No contacts associated.</div>
+                  ) : (
+                    <ul>
+                      {contacts.map(c => (
+                        <li key={c.contact_id}>{c.displayName}</li>
+                      ))}
+                    </ul>
+                  )}
+                  <h2>References</h2>
+                  <h2>Algorithm Description</h2>
+                  <h2>Algorithm Usage</h2>
+                </Prose>
+              </InpageBodyInner>
+            </InpageBody>
+          </StickyContainer>
         )}
-      </Fragment>
+      </Inpage>
     );
   }
 }
