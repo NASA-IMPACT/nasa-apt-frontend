b52c487727c81b2da6ce6b377b349da919b49a5e
Vitor George <vitor.george@gmail.com>
2019-07-17 13:24:16 +0100

    Add list of sections to ATBD View (non-extensive).

--- src/components/atbds/View.js
+++ src/components/atbds/View.js
@@ -9,11 +9,35 @@ class AtbdView extends Component {
 
   render() {
     const { atbd } = this.props;
+
+    if (!atbd) return <div>ATBD loading or not found.</div>;
+
+    const { contacts } = atbd;
+
     return (
       <Fragment>
         {atbd && (
           <Fragment>
             <h1>{atbd.title}</h1>
+            <h2>Identifying Information</h2>
+            <h3>General</h3>
+            <h3>Citation</h3>
+            <h2>Introduction</h2>
+            <h3>Introduction</h3>
+            <h3>Historical Perspective</h3>
+            <h2>Contacts</h2>
+            {contacts && contacts.length === 0 ? (
+              <div>No contacts associated.</div>
+            ) : (
+              <ul>
+                {contacts.map(c => (
+                  <li key={c.contact_id}>{c.displayName}</li>
+                ))}
+              </ul>
+            )}
+            <h2>References</h2>
+            <h2>Algorithm Description</h2>
+            <h2>Algorithm Usage</h2>
           </Fragment>
         )}
       </Fragment>
