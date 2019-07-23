ce6b4a51191de77a212a0aa1d20ce231610d6edb
Daniel da Silva <daniel@developmentseed.org>
2019-07-23 14:42:06 +0100

    Exclude specific actions

--- src/store/globalLoadingMiddleware.js
+++ src/store/globalLoadingMiddleware.js
@@ -4,11 +4,20 @@ import * as actions from '../actions/actions';
 const globalLoadingMiddleware = store => next => async (action) => {
   const { type } = action;
 
+  const excludedActions = [
+    'UPLOAD_JSON',
+    'SERIALIZE_DOCUMENT',
+    'CHECK_PDF',
+    'CHECK_HTML'
+  ];
+
   // Get all the actions of the async type by checking if they end in _SUCCESS
-  // filter.map = reduce
+  // .filter().map() = reduce()
   const asyncActions = Object.keys(types).reduce((acc, k) => {
     if (!k.endsWith('_SUCCESS')) return acc;
-    return [...acc, k.match(/(.*)_SUCCESS$/)[1]];
+    const name = k.match(/(.*)_SUCCESS$/)[1];
+    if (excludedActions.includes(name)) return acc;
+    return [...acc, name];
   }, []);
 
   asyncActions.forEach((act) => {
