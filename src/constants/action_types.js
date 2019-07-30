28d7a61eddc53a63a430ec1e8e2489acad2f33d3
Daniel da Silva <daniel@developmentseed.org>
2019-07-30 18:11:47 +0100

    Fix and exclude actions from loading
    SERIALIZE_PDF
    SERIALIZE_HTML

--- src/constants/action_types.js
+++ src/constants/action_types.js
@@ -54,10 +54,8 @@ const types = createTypes([
   ...async('FETCH_STATIC'),
   ...async('UPLOAD_JSON'),
   ...async('SERIALIZE_DOCUMENT'),
-  ...async('SERIALIZE_PDF_SUCCESS'),
-  ...async('SERIALIZE_PDF_FAIL'),
-  ...async('SERIALIZE_HTML_SUCCESS'),
-  ...async('SERIALIZE_HTML_FAIL'),
+  ...async('SERIALIZE_PDF'),
+  ...async('SERIALIZE_HTML'),
   ...async('CHECK_PDF'),
   ...async('CHECK_HTML'),
 
