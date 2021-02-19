a6cfbcbd08d3e5f24ce229bc8a77d7bfd0cbfe47
Ashish Acharya <ashish.acharya14@gmail.com>
2021-02-19 09:12:53 -0600

    Remove outdated contact information on the about page
    In the future, this will be replaced by the Feedback module.

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -35,7 +35,6 @@ class About extends Component {
                 This is the <strong>Algorithm Publication Tool</strong> prototype.<br />
                 We have endeavored to make it as easy as possible to create new ATBDs, and welcome all feedback.
               </p>
-              <p>Please feel free to <a href="mailto:alyssa@developmentseed.org?Subject=APT Prototype Feedback" target="_top">send us an e-mail</a>.</p>
             </AboutProse>
           </InpageBodyInner>
         </InpageBody>
