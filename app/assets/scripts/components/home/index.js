ad3232c1b63acdbc9a4a6992ceb7d1c0c3972a7b
Ricardo Mestre <ricardo@duplos.org>
2021-03-17 19:53:12 +0000

    Restructure homepage

--- app/assets/scripts/components/home/index.js
+++ app/assets/scripts/components/home/index.js
@@ -1,14 +1,46 @@
 import React from 'react';
-import Constrainer from '../../styles/constrainer';
+import styled from 'styled-components';
 
 import App from '../common/app';
 
+import {
+  Inpage,
+  InpageHeader,
+  InpageHeadline,
+  InpageTitle,
+  InpageBody
+} from '../../styles/inpage';
+
+import Constrainer from '../../styles/constrainer';
+import Prose from '../../styles/typography/prose';
+
+const InpageBodyScroll = styled(InpageBody)`
+  padding: 0;
+  overflow: auto;
+
+  ${Constrainer} {
+    padding-top: 3rem;
+    padding-bottom: 30rem;
+  }
+`;
+
 function Home() {
   return (
-    <App pageTitle='Home'>
-      <Constrainer>
-        <p>Hello</p>
-      </Constrainer>
+    <App pageTitle='Welcome'>
+      <Inpage>
+        <InpageHeader>
+          <InpageHeadline>
+            <InpageTitle>Welcome</InpageTitle>
+          </InpageHeadline>
+        </InpageHeader>
+        <InpageBodyScroll>
+          <Constrainer>
+            <Prose>
+              <p>Hello world!</p>
+            </Prose>
+          </Constrainer>
+        </InpageBodyScroll>
+      </Inpage>
     </App>
   );
 }
