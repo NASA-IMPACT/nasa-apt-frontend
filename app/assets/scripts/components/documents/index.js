1dc000e66db424a6ba27bbec12645893dd3f05d6
Daniel da Silva <danielfdsilva@gmail.com>
2021-03-23 19:26:12 +0000

    Draft implementation of documents hub

--- app/assets/scripts/components/documents/index.js
+++ app/assets/scripts/components/documents/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import { Button } from '@devseed-ui/button';
+import { GlobalLoading } from '@devseed-ui/global-loading';
 
 import App from '../common/app';
 import {
@@ -12,7 +13,8 @@ import {
   InpageBody
 } from '../../styles/inpage';
 import Constrainer from '../../styles/constrainer';
-import Prose from '../../styles/typography/prose';
+import DropdownMenu from '../common/dropdown-menu';
+import { Link } from '../../styles/clean/link';
 
 import { useAtbds } from '../../context/atbds-list';
 
@@ -27,14 +29,15 @@ const InpageBodyScroll = styled(InpageBody)`
 `;
 
 function Documents() {
-  const context = useAtbds();
+  const { fetchAtbds, atbds } = useAtbds();
 
   useEffect(() => {
-    context.fetchAtbds();
+    fetchAtbds();
   }, []);
 
   return (
     <App pageTitle='Documents'>
+      {atbds.status === 'loading' && <GlobalLoading />}
       <Inpage>
         <InpageHeader>
           <InpageHeadline>
@@ -48,9 +51,57 @@ function Documents() {
         </InpageHeader>
         <InpageBodyScroll>
           <Constrainer>
-            <Prose>
-              <p>Hello world!</p>
-            </Prose>
+            {atbds.status === 'succeeded' && atbds.data?.length && (
+              <table>
+                <thead>
+                  <tr>
+                    <th colSpan={2}>Name</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {atbds.data.map((atbd) => {
+                    const atbdVersions = [...atbd.versions].reverse();
+                    const lastVersion = atbdVersions[0];
+
+                    const versionMenu = {
+                      id: 'versions',
+                      items: atbdVersions.map((v) => ({
+                        id: v.version,
+                        label: v.version,
+                        title: `View ${v.version} page`,
+                        as: Link,
+                        to: `/documents/${atbd.alias || atbd.id}/${v.version}`
+                      }))
+                    };
+
+                    return (
+                      <tr key={atbd.id}>
+                        <td>
+                          <p>
+                            <Link
+                              to={`/documents/${atbd.alias || atbd.id}/${
+                                lastVersion.version
+                              }`}
+                            >
+                              {atbd.title}
+                            </Link>{' '}
+                            ({lastVersion.status})
+                          </p>
+
+                          <DropdownMenu
+                            menu={versionMenu}
+                            triggerLabel={lastVersion.version}
+                            withChevron
+                            dropTitle='Atbd versions'
+                          />
+                        </td>
+                        <td>Actions</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            )}
           </Constrainer>
         </InpageBodyScroll>
       </Inpage>
