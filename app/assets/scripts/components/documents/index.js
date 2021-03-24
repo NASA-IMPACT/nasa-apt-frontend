4c577a00f991718b5e870dda82c6b15794a0b012
Ricardo Mestre <ricardo@duplos.org>
2021-03-24 10:39:24 +0000

    Structure and style documents hub list

--- app/assets/scripts/components/documents/index.js
+++ app/assets/scripts/components/documents/index.js
@@ -1,8 +1,11 @@
 import React, { useEffect } from 'react';
-import styled from 'styled-components';
 import { Button } from '@devseed-ui/button';
 
 import App from '../common/app';
+import Status from '../common/status';
+
+import { Link } from '../../styles/clean/link';
+
 import {
   Inpage,
   InpageHeader,
@@ -12,19 +15,22 @@ import {
   InpageBody
 } from '../../styles/inpage';
 import Constrainer from '../../styles/constrainer';
-import Prose from '../../styles/typography/prose';
+
+import {
+  HubList,
+  HubListItem,
+  HubEntry,
+  HubEntryHeader,
+  HubEntryHeadline,
+  HubEntryTitle,
+  HubEntryMeta,
+  HubEntryDetails,
+  HubEntryHeadNav,
+  HubEntryBreadcrumbMenu,
+  HubEntryActions
+} from '../../styles/hub';
 
 import { useAtbds } from '../../context/atbds-list';
-
-const InpageBodyScroll = styled(InpageBody)`
-  padding: 0;
-  overflow: auto;
-
-  ${Constrainer} {
-    padding-top: 3rem;
-    padding-bottom: 30rem;
-  }
-`;
 
 function Documents() {
   const context = useAtbds();
@@ -46,13 +52,116 @@ function Documents() {
             </Button>
           </InpageActions>
         </InpageHeader>
-        <InpageBodyScroll>
+        <InpageBody>
           <Constrainer>
-            <Prose>
-              <p>Hello world!</p>
-            </Prose>
+            <HubList>
+              <HubListItem>
+                <HubEntry>
+                  <HubEntryHeader>
+                    <HubEntryHeadline>
+                      <HubEntryTitle>
+                        <Link to='/' title='View document'>
+                          GPM Integrated Multi-Satellite Retrievals for GPM
+                          (IMERG) Algorithm Theoretical Basis Document
+                        </Link>
+                      </HubEntryTitle>
+                      <HubEntryHeadNav role='navigation'>
+                        <HubEntryBreadcrumbMenu>
+                          <li>
+                            <strong>V1.0</strong>
+                          </li>
+                        </HubEntryBreadcrumbMenu>
+                      </HubEntryHeadNav>
+                    </HubEntryHeadline>
+                    <HubEntryMeta>
+                      <dt>Status</dt>
+                      <dd>
+                        <Status status='draft' completeness={64} />
+                      </dd>
+                      <dt>Discussion</dt>
+                      <dd>
+                        <a href='#' title='View threads'>
+                          2 unsolved threads
+                        </a>
+                      </dd>
+                    </HubEntryMeta>
+                    <HubEntryDetails>
+                      <dt>By</dt>
+                      <dd>George J. Huffman et al.</dd>
+                      <dt>On</dt>
+                      <dd>
+                        <time dateTime='2021-02-07'>Feb 7, 2021</time>
+                      </dd>
+                    </HubEntryDetails>
+                    <HubEntryActions>
+                      <Button
+                        to='/'
+                        variation='base-plain'
+                        useIcon='ellipsis-vertical'
+                        hideText
+                        title='Show options'
+                      >
+                        Options
+                      </Button>
+                    </HubEntryActions>
+                  </HubEntryHeader>
+                </HubEntry>
+              </HubListItem>
+              <HubListItem>
+                <HubEntry>
+                  <HubEntryHeader>
+                    <HubEntryHeadline>
+                      <HubEntryTitle>
+                        <Link to='/' title='View document'>
+                          GPM Integrated Multi-Satellite Retrievals for GPM
+                          (IMERG) Algorithm Theoretical Basis Document
+                        </Link>
+                      </HubEntryTitle>
+                      <HubEntryHeadNav role='navigation'>
+                        <HubEntryBreadcrumbMenu>
+                          <li>
+                            <strong>V1.0</strong>
+                          </li>
+                        </HubEntryBreadcrumbMenu>
+                      </HubEntryHeadNav>
+                    </HubEntryHeadline>
+                    <HubEntryMeta>
+                      <dt>Status</dt>
+                      <dd>
+                        <Status status='draft' completeness={64} />
+                      </dd>
+                      <dt>Discussion</dt>
+                      <dd>
+                        <a href='#' title='View threads'>
+                          2 unsolved threads
+                        </a>
+                      </dd>
+                    </HubEntryMeta>
+                    <HubEntryDetails>
+                      <dt>By</dt>
+                      <dd>George J. Huffman et al.</dd>
+                      <dt>On</dt>
+                      <dd>
+                        <time dateTime='2021-02-07'>Feb 7, 2021</time>
+                      </dd>
+                    </HubEntryDetails>
+                    <HubEntryActions>
+                      <Button
+                        to='/'
+                        variation='base-plain'
+                        useIcon='ellipsis-vertical'
+                        hideText
+                        title='Show options'
+                      >
+                        Options
+                      </Button>
+                    </HubEntryActions>
+                  </HubEntryHeader>
+                </HubEntry>
+              </HubListItem>
+            </HubList>
           </Constrainer>
-        </InpageBodyScroll>
+        </InpageBody>
       </Inpage>
     </App>
   );
