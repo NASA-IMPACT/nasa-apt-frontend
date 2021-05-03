f4bab4fb0c665c16b9b3cf8f62e5c1324bf5cfeb
Ricardo Mestre <ricardo@duplos.org>
2021-05-03 16:59:13 +0100

    Restructure and restyle equation preview

--- app/assets/scripts/components/slate/plugins/equation/equation-editor.js
+++ app/assets/scripts/components/slate/plugins/equation/equation-editor.js
@@ -4,10 +4,55 @@ import styled from 'styled-components';
 import { Node } from 'slate';
 import { useReadOnly } from 'slate-react';
 import { BlockMath } from 'react-katex';
+import { glsp, themeVal } from '@devseed-ui/theme-provider';
+import { visuallyHidden } from '@devseed-ui/theme-provider';
+import { headingAlt } from '@devseed-ui/typography';
+import collecticon from '@devseed-ui/collecticons';
 
 const EquationInput = styled.p`
-  text-align: center;
   font-family: monospace;
+`;
+
+const EquationPreview = styled.aside`
+  position: relative;
+  background: ${themeVal('color.surface')};
+  padding: ${glsp(1.5)};
+  border-radius: ${themeVal('shape.rounded')};
+  box-shadow: 0 0 0 1px ${themeVal('color.baseAlphaD')};
+  margin-top: ${glsp()};
+
+  &::before {
+    ${collecticon('triangle-up')}
+    position: absolute;
+    bottom: 100%;
+    left: 1rem;
+    z-index: 10;
+    color: ${themeVal('color.surface')};
+    text-shadow: 0 -1.5px 0 ${themeVal('color.baseAlphaD')};
+    width: 1rem;
+    font-size: 1rem;
+    line-height: 1;
+    text-align: center;
+    transform: translateY(0.35rem);
+    pointer-events: none;
+  }
+`;
+
+const EquationPreviewTitle = styled.h6`
+  ${headingAlt()}
+  font-size: 0.75rem;
+  line-height: 1rem;
+  margin: ${glsp(-1, 0, 0, -1)};
+
+  span {
+    ${visuallyHidden()}
+  }
+`;
+
+const EquationPreviewBody = styled.div`
+  .katex-display {
+    margin: 0;
+  }
 `;
 
 export default function EquationEditor(props) {
@@ -23,8 +68,15 @@ export default function EquationEditor(props) {
     <div {...attributes}>{equation}</div>
   ) : (
     <div {...attributes}>
-      <EquationInput spellCheck={false}>{children}</EquationInput>
-      <div contentEditable={false}>{equation}</div>
+      <EquationInput spellCheck={false}>
+        <code>{children}</code>
+      </EquationInput>
+      <EquationPreview contentEditable={false}>
+        <EquationPreviewTitle>
+          <span>Equation</span> preview
+        </EquationPreviewTitle>
+        <EquationPreviewBody>{equation}</EquationPreviewBody>
+      </EquationPreview>
     </div>
   );
 }
