1b5530b46f80215cb4ffb9dc916d84c8fd7029c6
Daniel da Silva <danielfdsilva@gmail.com>
2021-06-02 10:26:36 +0100

    Update staging url

--- app/assets/scripts/config/staging.js
+++ app/assets/scripts/config/staging.js
@@ -1,4 +1,4 @@
 // module exports is required to be able to load from gulpfile.
 module.exports = {
-  apiUrl: 'https://nuo783m1th.execute-api.us-east-1.amazonaws.com'
+  apiUrl: 'https://95623q3plb.execute-api.us-east-1.amazonaws.com/v2'
 };
