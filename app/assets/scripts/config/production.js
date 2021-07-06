b4c8805e4341906158c7d70937e9ccf5df7a8e49
Daniel da Silva <danielfdsilva@gmail.com>
2021-07-06 18:14:48 +0100

    Add production config
    [ci skip]

--- app/assets/scripts/config/production.js
+++ app/assets/scripts/config/production.js
@@ -2,17 +2,18 @@
 module.exports = {
   appTitle: 'Algorithm Publication Tool',
   appDescription: 'Algorithm Publication Tool - ATBD management.',
-  apiUrl: '',
+  apiUrl: 'https://fulja173cc.execute-api.us-east-2.amazonaws.com/v2',
   auth: {
     // DOCS: https://docs.amplify.aws/lib/auth/start/q/platform/js#re-use-existing-authentication-resource
     // Amazon Cognito Region
-    region: '',
+    region: 'us-east-2',
     // Amazon Cognito User Pool ID
-    userPoolId: '',
+    userPoolId: 'us-east-2_dsHdmbBUK',
     // Amazon Cognito Web Client ID (26-char alphanumeric string)
-    userPoolWebClientId: '',
+    userPoolWebClientId: '66kd4hhbm7cb5suifelj34btki',
     // Manually set the authentication flow type. Default is 'USER_SRP_AUTH'
     authenticationFlowType: 'USER_PASSWORD_AUTH'
   },
-  hostedAuthUi: ''
+  hostedAuthUi:
+    'https://nasa-apt-api-lambda-prod.auth.us-east-2.amazoncognito.com'
 };
