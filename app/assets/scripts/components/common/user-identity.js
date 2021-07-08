15dd6edc255e2f30eadcbe5702bc990558a035a9
Daniel da Silva <danielfdsilva@gmail.com>
2021-07-08 11:07:22 +0100

    Create user identity element

---
+++ app/assets/scripts/components/common/user-identity.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+import T from 'prop-types';
+import { glsp, truncated } from '@devseed-ui/theme-provider';
+
+import Pill from './pill';
+import UserImage from './user-image';
+
+const UserIdentityCmp = (props) => {
+  const { user, role, ...rest } = props;
+
+  return (
+    <div {...rest} title={user.name}>
+      <UserImage user={user} />
+      <strong>{user.name}</strong>
+      {role && <Pill>{role}</Pill>}
+    </div>
+  );
+};
+
+UserIdentityCmp.propTypes = {
+  user: T.object,
+  role: T.string
+};
+
+const UserIdentity = styled(UserIdentityCmp)`
+  display: grid;
+  grid-template-columns: min-content 1fr;
+  grid-auto-columns: min-content;
+  grid-auto-flow: column;
+  grid-gap: ${glsp(0.5)};
+  align-items: center;
+
+  > strong {
+    ${truncated()}
+  }
+`;
+
+export default UserIdentity;
