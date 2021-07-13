28c0836ee10482909a1f1829a642b89988ef4619
Daniel da Silva <danielfdsilva@gmail.com>
2021-07-13 17:48:14 +0100

    Convert contributors menu to dropdown

--- app/assets/scripts/components/dashboard/contributors-menu.js
+++ app/assets/scripts/components/dashboard/contributors-menu.js
@@ -1,32 +1,41 @@
 import React from 'react';
 import T from 'prop-types';
 import styled from 'styled-components';
-import { glsp, rgba, themeVal } from '@devseed-ui/theme-provider';
-import collecticon from '@devseed-ui/collecticons';
-import { headingAlt } from '@devseed-ui/typography';
+import { glsp, themeVal } from '@devseed-ui/theme-provider';
+import { Heading } from '@devseed-ui/typography';
+import Dropdown from '@devseed-ui/dropdown';
+import { Button } from '@devseed-ui/button';
 
-import Tip from '../common/tooltip';
-import Pill from '../common/pill';
-import UserImage from '../common/user-image';
-
-const ContributorsLabel = styled.span`
-  font-size: 0.875rem;
-
-  ::before {
-    ${collecticon('user')}
-    margin-right: ${glsp(0.25)};
-  }
-`;
+import UserIdentity from '../common/user-identity';
 
 // Drop content elements.
-const DropTitle = styled.h5`
-  ${headingAlt()}
-  color: ${rgba(themeVal('color.base'), 0.64)};
-  font-size: 0.875rem;
-  line-height: 1rem;
-  margin: ${glsp(-1, -1, 1, -1)};
+
+const DropdownContributors = styled(Dropdown)`
+  max-width: 20rem;
+`;
+
+const DropHeader = styled.header`
+  display: grid;
+  grid-gap: ${glsp()};
+  box-shadow: 0 1px 0 0 ${themeVal('color.baseAlphaC')};
+  margin: ${glsp(-1, -1, 0, -1)};
   padding: ${glsp()};
-  box-shadow: 0 ${themeVal('layout.border')} 0 0 ${themeVal('color.baseAlphaC')};
+`;
+
+const DropHeadline = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  align-items: center;
+`;
+
+const DropActions = styled.div`
+  display: flex;
+  margin-left: auto;
+`;
+
+const DropTitle = styled(Heading)`
+  flex-basis: 100%;
+  margin: 0;
 `;
 
 const ContributorsList = styled.dl`
@@ -43,23 +52,9 @@ const ContributorsList = styled.dl`
   }
 `;
 
-const ContributorEntrySelf = styled.div`
-  display: grid;
-  grid-template-columns: min-content 1fr min-content;
-  grid-gap: ${glsp(0.5)};
-  align-items: center;
-`;
-
 const ContributorEntry = (props) => {
   const { isLead, user } = props;
-
-  return (
-    <ContributorEntrySelf>
-      <UserImage user={user} />
-      <span>{user.name}</span>
-      {isLead && <Pill>Lead</Pill>}
-    </ContributorEntrySelf>
-  );
+  return <UserIdentity user={user} role={isLead && 'Lead'} />;
 };
 
 ContributorEntry.propTypes = {
@@ -69,50 +64,74 @@ ContributorEntry.propTypes = {
 
 const ContributorsMenuContent = () => {
   return (
-    <div>
-      <DropTitle>Contributors</DropTitle>
-      <ContributorsList>
-        <dt>Authors</dt>
-        <dd>
-          <ul>
-            <li>
-              <ContributorEntry user={{ name: 'Sean Riley' }} isLead />
-            </li>
-            <li>
-              <ContributorEntry user={{ name: 'Monica Anderson' }} />
-            </li>
-            <li>
-              <ContributorEntry user={{ name: 'Carlos Maria' }} />
-            </li>
-          </ul>
-        </dd>
-        <dt>Reviewers</dt>
-        <dd>
-          <ul>
-            <li>
-              <ContributorEntry user={{ name: 'Bruce Wayne' }} />
-            </li>
-          </ul>
-        </dd>
-      </ContributorsList>
-    </div>
+    <React.Fragment>
+      <DropHeader>
+        <DropHeadline>
+          <DropActions>
+            <Button
+              variation='base-plain'
+              title='Open dropdown'
+              useIcon='pencil'
+              size='small'
+              hideText
+              // onClick={onCancelButtonClick}
+              data-dropdown='click.close'
+            >
+              Edit contributors
+            </Button>
+          </DropActions>
+          <DropTitle size='xsmall'>Contributors</DropTitle>
+        </DropHeadline>
+      </DropHeader>
+      <div>
+        <ContributorsList>
+          <dt>Authors</dt>
+          <dd>
+            <ul>
+              <li>
+                <ContributorEntry user={{ name: 'Sean Riley' }} isLead />
+              </li>
+              <li>
+                <ContributorEntry user={{ name: 'Monica Anderson' }} />
+              </li>
+              <li>
+                <ContributorEntry user={{ name: 'Carlos Maria' }} />
+              </li>
+            </ul>
+          </dd>
+          <dt>Reviewers</dt>
+          <dd>
+            <ul>
+              <li>
+                <ContributorEntry user={{ name: 'Bruce Wayne' }} />
+              </li>
+            </ul>
+          </dd>
+        </ContributorsList>
+      </div>
+    </React.Fragment>
   );
 };
 
 export function ContributorsMenu() {
   return (
-    <Tip
-      position='top'
-      html={<ContributorsMenuContent />}
-      theme='apt-drop'
-      arrow={false}
-      animateFill={false}
-      animation='shift'
-      interactive
-      useContext
+    <DropdownContributors
+      alignment='center'
+      direction='down'
+      triggerElement={(props) => (
+        <Button
+          variation='base-plain'
+          title='Open dropdown'
+          useIcon='user'
+          size='small'
+          {...props}
+        >
+          Sean Riley +3
+        </Button>
+      )}
     >
-      <ContributorsLabel>Sean Riley +3</ContributorsLabel>
-    </Tip>
+      <ContributorsMenuContent />
+    </DropdownContributors>
   );
 }
 
