be13277a6cff0d46ecbd880f4d7e06d823e364e9
Ricardo Mestre <ricardo@duplos.org>
2021-07-16 11:23:21 +0100

    Tweak document meta inner spacing

--- app/assets/scripts/styles/inpage.js
+++ app/assets/scripts/styles/inpage.js
@@ -69,7 +69,7 @@ export const InpageHeadHgroup = styled.div`
 
 export const InpageMeta = styled.ul`
   display: grid;
-  grid-gap: ${glsp(0.5)};
+  grid-gap: ${glsp(1)};
   align-items: center;
   grid-auto-columns: minmax(min-content, max-content);
   font-size: 1rem;
