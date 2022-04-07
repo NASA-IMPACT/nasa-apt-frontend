367e40ba3521cf734dd75b48a9b586a101788c64
Oliver Roick <oliver@roick.email>
2022-04-07 10:09:03 +0200

    Add recover functionality

--- app/assets/scripts/components/documents/single-edit/local-store.js
+++ app/assets/scripts/components/documents/single-edit/local-store.js
@@ -1,7 +1,11 @@
-import { useEffect, useRef } from 'react';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
+import T from 'prop-types';
 import { useFormikContext } from 'formik';
+import styled from 'styled-components';
 import { useParams } from 'react-router';
 import defaultsDeep from 'lodash.defaultsdeep';
+import { toast } from 'react-toastify';
+import { Button as BaseButton } from '@devseed-ui/button';
 
 export class LocalAtbdStorage {
   constructor() {
@@ -45,12 +49,74 @@ export class LocalAtbdStorage {
   }
 }
 
+const Button = styled(BaseButton)`
+  margin-top: 0.5rem;
+  margin-right: 0.5rem;
+`;
+
+function RecoverToast({ recoverData, clearData, closeToast }) {
+  const handleRecover = () => {
+    recoverData();
+    closeToast();
+  };
+
+  const handleClear = () => {
+    clearData();
+    closeToast();
+  };
+
+  return (
+    <div>
+      <p>
+        We have recovered data that you had inserted. Do you want to restore the
+        data?
+      </p>
+      <Button
+        variation='primary-raised-light'
+        size='small'
+        type='button'
+        onClick={handleRecover}
+      >
+        Recover
+      </Button>
+      <Button
+        variation='primary-raised-light'
+        size='small'
+        type='button'
+        onClick={handleClear}
+      >
+        Discard
+      </Button>
+    </div>
+  );
+}
+
+RecoverToast.propTypes = {
+  recoverData: T.func.isRequired,
+  clearData: T.func.isRequired,
+  closeToast: T.func.isRequired
+};
+
 export function LocalStore({ atbd }) {
   const isInitialized = useRef();
   const { values, setValues, dirty } = useFormikContext();
   const { step } = useParams();
 
   const stepId = step || 'identifying_information';
+  const [displayRecoverToast, setDisplayRecoverToast] = useState(false);
+
+  const recoverData = useCallback(() => {
+    const localAtbdStorage = new LocalAtbdStorage();
+    const localValues = localAtbdStorage.getAtbd(atbd, stepId);
+    setValues(defaultsDeep(localValues, values));
+    setDisplayRecoverToast(false);
+  }, [atbd, values, setValues, stepId]);
+
+  const clearData = useCallback(() => {
+    const localAtbdStorage = new LocalAtbdStorage();
+    localAtbdStorage.removeAtbd(atbd, stepId);
+    setDisplayRecoverToast(false);
+  }, [atbd, stepId]);
 
   useEffect(() => {
     const localAtbdStorage = new LocalAtbdStorage();
@@ -64,12 +130,33 @@ export function LocalStore({ atbd }) {
       if (atbd) {
         const localValues = localAtbdStorage.getAtbd(atbd, stepId);
         if (localValues) {
-          setValues(defaultsDeep(localValues, values));
+          setDisplayRecoverToast(true);
         }
         isInitialized.current = true;
       }
     }
   }, [atbd, stepId, values, setValues, dirty]);
 
+  useEffect(() => {
+    if (displayRecoverToast) {
+      toast.info(
+        ({ closeToast }) => (
+          <RecoverToast
+            recoverData={recoverData}
+            clearData={clearData}
+            closeToast={closeToast}
+          />
+        ),
+        {
+          autoClose: false
+        }
+      );
+    }
+  }, [displayRecoverToast, clearData, recoverData]);
+
   return null;
 }
+
+LocalStore.propTypes = {
+  atbd: T.object.isRequired
+};
