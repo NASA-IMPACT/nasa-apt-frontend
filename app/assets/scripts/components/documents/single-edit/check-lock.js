99ff35719d4c9544fca84b155c7c7c7d1e6b95de
Oliver Roick <oliver@roick.email>
2022-06-07 14:47:47 +1000

    Check lock on edit-form load and display modal

---
+++ app/assets/scripts/components/documents/single-edit/check-lock.js
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from 'react';
+import T from 'prop-types';
+
+import { Modal, ModalFooter } from '@devseed-ui/modal';
+import { Button } from '@devseed-ui/button';
+
+import { axiosAPI } from '../../../utils/axios';
+
+const checkLock = (alias, version, userToken) => {
+  const headers = userToken
+    ? {
+        headers: {
+          Authorization: `Bearer ${userToken}`
+        }
+      }
+    : {};
+
+  return axiosAPI({
+    url: `/atbds/${alias}/versions/${version}/lock`,
+    method: 'post',
+    ...headers
+  });
+};
+
+function CheckLock({ id, version, user }) {
+  const [showModal, setShowModal] = useState(false);
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    checkLock(id, version, user.accessToken).catch((error) => {
+      setMessage(error.response.data.detail);
+      setShowModal(true);
+    });
+  }, [id, version, user.accessToken]);
+
+  return (
+    <Modal
+      id='modal'
+      size='small'
+      revealed={showModal}
+      // onCloseClick={onClose}
+      title='ATDB is locked'
+      content={
+        <>
+          <p>
+            {message} You can unlock this ATBD version and start editing but the
+            current editing user will loose their changes.
+          </p>
+          <p>Do you want to unlock the ATDB version?</p>
+        </>
+      }
+      renderFooter={() => (
+        <ModalFooter>
+          <Button
+            type='button'
+            variation='primary-raised-dark'
+            useIcon='tick--small'
+            onClick={() => {}}
+          >
+            Unlock
+          </Button>
+          <Button
+            type='button'
+            variation='base-raised-light'
+            useIcon='xmark--small'
+            onClick={() => {}}
+          >
+            Cancel
+          </Button>
+        </ModalFooter>
+      )}
+    />
+  );
+}
+
+CheckLock.propTypes = {
+  id: T.string.isRequired,
+  version: T.string.isRequired,
+  user: T.shape({
+    accessToken: T.string.isRequired
+  })
+};
+
+export default CheckLock;
