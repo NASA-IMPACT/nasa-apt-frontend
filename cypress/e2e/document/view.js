a19aea0a4c6193945eba586243a297788dd55e1a
Oliver Roick <oliver@roick.email>
2022-08-15 14:00:23 +0200

    Add view tests to unauthorized users

--- cypress/e2e/document/view.js
+++ cypress/e2e/document/view.js
@@ -7,11 +7,12 @@ describe('Document View', () => {
   beforeEach(() => {
     cy.intercept('GET', '/v2/atbds?role=owner', atbds);
     cy.intercept('GET', '/v2/threads/stats?atbds=1_v1.1', stats);
-    cy.intercept('GET', '/v2/atbds/test-atbd-1', atbd);
-    cy.intercept('GET', '/v2/atbds/test-atbd-1/versions/v1.1', atbdVersions);
   });
 
-  it('Displays ATBD', () => {
+  it('Displays ATBD to authorised user', () => {
+    cy.intercept('GET', '/v2/atbds/test-atbd-1', atbd);
+    cy.intercept('GET', '/v2/atbds/test-atbd-1/versions/v1.1', atbdVersions);
+
     cy.login();
     cy.visit('/documents/test-atbd-1/v1.1');
 
@@ -26,5 +27,42 @@ describe('Document View', () => {
       .contains(
         /A brief and interesting overview of the themse presented in this document, but this time, without jargon/
       );
+  });
+
+  it('Shows error page to anonymous user', () => {
+    cy.intercept('GET', '/v2/atbds/test-atbd-1', {
+      statusCode: 404,
+      body: {
+        detail: 'No atbds found'
+      }
+    });
+    cy.intercept('GET', '/v2/atbds/test-atbd-1/versions/v1.1', {
+      statusCode: 403,
+      body: {
+        detail: 'View for ATBD Version is not allowed'
+      }
+    });
+
+    cy.visit('/documents/test-atbd-1/v1.1');
+    cy.contains('Page not found');
+  });
+
+  it('Shows error page to unauthoriszed user', () => {
+    cy.intercept('GET', '/v2/atbds/test-atbd-1', {
+      statusCode: 404,
+      body: {
+        detail: 'No atbds found'
+      }
+    });
+    cy.intercept('GET', '/v2/atbds/test-atbd-1/versions/v1.1', {
+      statusCode: 403,
+      body: {
+        detail: 'View for ATBD Version is not allowed'
+      }
+    });
+
+    cy.login();
+    cy.visit('/documents/test-atbd-1/v1.1');
+    cy.contains('Page not found');
   });
 });
