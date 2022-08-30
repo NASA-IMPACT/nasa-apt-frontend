7af902db70dc776a2daca63330c752d427024f16
Timothy Ellersiek <timothy@developmentseed.org>
2022-08-30 21:04:16 +0200

    updated cognito signup url

--- app/assets/scripts/config/staging.js
+++ app/assets/scripts/config/staging.js
@@ -13,6 +13,5 @@ module.exports = {
     // Manually set the authentication flow type. Default is 'USER_SRP_AUTH'
     authenticationFlowType: 'USER_PASSWORD_AUTH'
   },
-  hostedAuthUi:
-    'https://nasa-apt-api-lambda-staging.auth.us-east-1.amazoncognito.com'
+  hostedAuthUi: 'https://nasa-apt-api-staging.auth.us-east-1.amazoncognito.com'
 };
