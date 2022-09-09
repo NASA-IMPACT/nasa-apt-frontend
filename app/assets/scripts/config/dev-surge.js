d0e2880140d59689ae628690cfd6ff5cb5491111
Matt Robinson <pacificrobinson@gmail.com>
2022-09-08 20:23:19 -0400

    update dev surge with new APT stack credentials

--- app/assets/scripts/config/dev-surge.js
+++ app/assets/scripts/config/dev-surge.js
@@ -2,18 +2,18 @@
 module.exports = {
     appTitle: 'Algorithm Publishing Tool',
     appDescription: 'Algorithm Publishing Tool - Publish algorithms.',
-    apiUrl: '',
+    apiUrl: 'https://abglctw3j2.execute-api.us-east-1.amazonaws.com',
     auth: {
       // DOCS: https://docs.amplify.aws/lib/auth/start/q/platform/js#re-use-existing-authentication-resource
       // Amazon Cognito Region
       region: 'us-east-1',
       // Amazon Cognito User Pool ID
-      userPoolId: '',
+      userPoolId: 'us-east-1_H2fR1djlP',
       // Amazon Cognito Web Client ID (26-char alphanumeric string)
-      userPoolWebClientId: '',
+      userPoolWebClientId: '21vc31gl0g9lf1ljvvfc70g25a',
       // Manually set the authentication flow type. Default is 'USER_SRP_AUTH'
       authenticationFlowType: 'USER_PASSWORD_AUTH'
     },
-    hostedAuthUi: '' // COGNITO HOSTED APP URL
+    hostedAuthUi: 'https://nasa-apt-api-dev.auth.us-east-1.amazoncognito.com' // COGNITO HOSTED APP URL
   };
   
