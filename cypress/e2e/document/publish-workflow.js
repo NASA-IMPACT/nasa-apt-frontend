af86c1bae5c38291426e89bc4a346164c87f59e4
Oliver Roick <oliver@roick.email>
2022-09-13 12:00:30 +1000

    Add cancel review request tests

--- cypress/e2e/document/publish-workflow.js
+++ cypress/e2e/document/publish-workflow.js
@@ -89,4 +89,83 @@ describe('Publish workflow', () => {
     cy.visit('/documents/test-atbd-1/v1.1');
     cy.contains(/Request review/).should('not.exist');
   });
+
+  it('owner cancels review request', () => {
+    const draftAtbdVersions = versionWithStatus(
+      atbdVersions,
+      'CLOSED_REVIEW_REQUESTED'
+    );
+
+    cy.intercept('GET', '/v2/atbds/test-atbd-1', atbd);
+    cy.intercept(
+      'GET',
+      '/v2/atbds/test-atbd-1/versions/v1.1',
+      draftAtbdVersions
+    );
+
+    cy.login('owner');
+    cy.visit('/documents/test-atbd-1/v1.1');
+
+    cy.intercept('POST', '/v2/events', atbdEvent).as('reviewRequest');
+    cy.contains(/Cancel request/).click();
+    cy.get('#the-toast').contains('Review request cancelled successfully');
+
+    cy.get('@reviewRequest').its('request.body').should('deep.equal', {
+      atbd_id: 'test-atbd-1',
+      version: 'v1.1',
+      action: 'cancel_closed_review_request',
+      payload: {}
+    });
+  });
+
+  it('owner cannot cancel review request if not review is not requested', () => {
+    const draftAtbdVersions = versionWithStatus(atbdVersions, 'DRAFT');
+
+    cy.intercept('GET', '/v2/atbds/test-atbd-1', atbd);
+    cy.intercept(
+      'GET',
+      '/v2/atbds/test-atbd-1/versions/v1.1',
+      draftAtbdVersions
+    );
+
+    cy.login('owner');
+    cy.visit('/documents/test-atbd-1/v1.1');
+    cy.contains(/Cancel request/).should('not.exist');
+  });
+
+  it('curator cannot cancel review request', () => {
+    const draftAtbdVersions = versionWithStatus(
+      atbdVersions,
+      'CLOSED_REVIEW_REQUESTED'
+    );
+
+    cy.intercept('GET', '/v2/atbds/test-atbd-1', atbd);
+    cy.intercept(
+      'GET',
+      '/v2/atbds/test-atbd-1/versions/v1.1',
+      draftAtbdVersions
+    );
+
+    cy.login('curator');
+    cy.visit('/documents/test-atbd-1/v1.1');
+    cy.contains(/Cancel request/).should('not.exist');
+  });
+
+  it('contributor cannot cancel review request', () => {
+    const draftAtbdVersions = versionWithStatus(
+      atbdVersions,
+      'CLOSED_REVIEW_REQUESTED'
+    );
+
+    cy.intercept('GET', '/v2/atbds/test-atbd-1', atbd);
+    cy.intercept(
+      'GET',
+      '/v2/atbds/test-atbd-1/versions/v1.1',
+      draftAtbdVersions
+    );
+
+    cy.login('contributor');
+    cy.visit('/documents/test-atbd-1/v1.1');
+    cy.contains(/Cancel request/).should('not.exist');
+  });
 });
