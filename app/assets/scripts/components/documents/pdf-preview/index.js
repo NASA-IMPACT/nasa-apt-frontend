bf3d5011454e6ad2a8add2bd34b4a5e513b07bb6
frozenhelium <ankit.mehta@togglecorp.com>
2023-03-15 11:22:41 +0545

    Update toc to include the user-defined headings in pdf-preview

--- app/assets/scripts/components/documents/pdf-preview/index.js
+++ app/assets/scripts/components/documents/pdf-preview/index.js
@@ -22,6 +22,8 @@ class AfterRenderHandler extends Handler {
   }
 
   beforeParsed(content) {
+    // Returns title element to be injected in the ToC
+    // Adds the dashed line and space for title and page number
     function getTitleElement(sectionId, sectionTitle) {
       const title = document.createElement('a');
       const text = document.createElement('span');
@@ -48,6 +50,8 @@ class AfterRenderHandler extends Handler {
 
     const tocElement = content.querySelector('#table-of-contents');
 
+    // This function iteratively finds the subheadings
+    // (h3, h4, h5, ...) in the content and creates ToC entry
     function generateSubHeadings(
       currentHeading,
       currentLevel,
@@ -58,6 +62,16 @@ class AfterRenderHandler extends Handler {
       const currentSubHeadings = [];
       let currentEl = currentHeading;
 
+      // We separately handle the user-defined subheadings, since they
+      // don't follow the pattern of other subheadings
+      const userDefinedHeadings = currentHeading.nextElementSibling.querySelectorAll(
+        `H${currentLevel + 1}`
+      );
+      currentSubHeadings.push(...Array.from(userDefinedHeadings));
+
+      // The subheadings are in the same hierarcy / nesting
+      // So, we find smaller sub-headings between current level of
+      // sub-headings.
       while (
         currentEl.nextElementSibling !== null &&
         currentEl.nextElementSibling.tagName !== `H${currentLevel}`
@@ -75,6 +89,8 @@ class AfterRenderHandler extends Handler {
         sectionContainer.append(subHeadingSections);
 
         currentSubHeadings.forEach((subHeading, i) => {
+          // Generating the heading number and injecting it to the content.
+          // So, it appears both in the ToC and in the content.
           const headingNumber = `${parentHeadingNumber}${i + 1}.`;
           if (subHeading.children[0]) {
             subHeading.children[0].prepend(`${headingNumber} `);
@@ -98,24 +114,36 @@ class AfterRenderHandler extends Handler {
       }
     }
 
+    // This function generates the main headings from h2 in the content
+    // and calls generateSubHeadings to find subheadings between 2 consecutive headings
     function generateHeading() {
       const sectionHeadings = content.querySelectorAll('h2');
-      Array.from(sectionHeadings).forEach((h, i) => {
+      Array.from(sectionHeadings).forEach((heading, i) => {
         const section = document.createElement('div');
         section.classList.add('toc-section');
         tocElement.append(section);
 
         const headingNumber = `${i + 1}.`;
-        if (h.children[0]) {
-          h.children[0].prepend(`${headingNumber} `);
+        if (heading.children[0]) {
+          // Inject the heading number to the content itself
+          // so that it appears both in the document heading
+          // and in the ToC
+          heading.children[0].prepend(`${headingNumber} `);
         }
 
-        const sectionTitle = getTitleElement(h.id, h.innerText);
+        const sectionTitle = getTitleElement(heading.id, heading.innerText);
         section.append(sectionTitle);
-        generateSubHeadings(h, 2, section, h.parentNode, headingNumber);
+        generateSubHeadings(
+          heading,
+          2,
+          section,
+          heading.parentNode,
+          headingNumber
+        );
       });
     }
 
+    // Starting from h2
     generateHeading(2, tocElement, content, undefined);
   }
 
@@ -165,15 +193,10 @@ function PdfPreview() {
     <ScrollAnchorProvider disabled>
       {atbd.status === 'loading' && <GlobalLoading />}
       {atbd.status === 'succeeded' && (
-        <div>
+        <>
           <div id='content' ref={contentRef}>
-            <div>
-              <TocHeader>Table of Contents</TocHeader>
-              <div
-                className='preview-table-of-content'
-                id='table-of-contents'
-              />
-            </div>
+            <TocHeader>Table of Contents</TocHeader>
+            <div className='preview-table-of-content' id='table-of-contents' />
             <DocumentContent
               className='preview-page-content'
               atbdData={atbd.data}
@@ -182,7 +205,7 @@ function PdfPreview() {
           </div>
           <div id='preview' ref={previewRef} />
           {previewReady && <div id='pdf-preview-ready' />}
-        </div>
+        </>
       )}
     </ScrollAnchorProvider>
   );
